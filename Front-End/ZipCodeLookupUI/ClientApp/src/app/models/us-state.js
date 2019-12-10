530ca487ca070f0d0967971bac3d8f190acaba67
Wassim Katbey <fv6124@wayne.edu>
2019-12-10 16:45:13 -0500

    Removing js files and bin files

--- Front-End/ZipCodeLookupUI/ClientApp/src/app/models/us-state.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var UsState = /** @class */ (function () {
-    function UsState() {
-    }
-    return UsState;
-}());
-exports.UsState = UsState;
-//# sourceMappingURL=us-state.js.map

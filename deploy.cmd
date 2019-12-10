01cbe104717bf8562f0a2186a471a8501fb0e0fb
Wassim Katbey <fv6124@wayne.edu>
2019-12-10 17:04:15 -0500

    Changed the deployment scripts

--- deploy.cmd
+++ deploy.cmd
@@ -1,11 +1,11 @@
- @echo off
-    IF "%DEPLOYMENT_PROJECT%" == "ui" (
-      deploy.ui.cmd
+@echo off
+IF "%DEPLOYMENT_PROJECT%" == "ui" (
+    deploy.ui.cmd
+) ELSE (
+    IF "%DEPLOYMENT_PROJECT%" == "api" (
+    deploy.api.cmd
     ) ELSE (
-      IF "%DEPLOYMENT_PROJECT%" == "api" (
-        deploy.api.cmd
-      ) ELSE (
-        echo DEPLOYMENT_PROJECT must be set to either 'ui' or 'api'
-        exit /b 1
-      )
+    echo You have to set the DEPLOYMENT_PROJECT setting to either "ui" or "api"
+    exit /b 1
     )
+)
